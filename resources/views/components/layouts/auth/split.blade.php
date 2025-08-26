a6610eeb564869879e401ac6da9be959925c0999
Mouton <MoutonH@bdv.local>
2025-08-26 02:03:23 +0200

    feat: wow what a ui

--- resources/views/components/layouts/auth/split.blade.php
+++ resources/views/components/layouts/auth/split.blade.php
@@ -6,23 +6,28 @@
     <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
         <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-e dark:border-neutral-800">
-                <div class="absolute inset-0 bg-neutral-900"></div>
+                <div class="absolute inset-0 ">
+                    <img src="{{asset('assets/Light 03.png')}}" alt="" class="absolute inset-0 h-full w-full object-cover opacity-60" />
+                </div>
                 <a href="{{ route('home') }}" class="relative z-20 flex items-center text-lg font-medium" wire:navigate>
                     <span class="flex h-10 w-10 items-center justify-center rounded-md">
                         <x-app-logo-icon class="me-2 h-7 fill-current text-white" />
                     </span>
-                    {{ config('app.name', 'Laravel') }}
+                    <span class="text-black"> {{ config('app.name', 'Laravel') }}</span>
+                   
                 </a>
 
-                @php
-                    [$message, $author] = str(Illuminate\Foundation\Inspiring::quotes()->random())->explode('-');
-                @endphp
+                    @php
+                        [$message, $author] = str(Illuminate\Foundation\Inspiring::quotes()->random())->explode('-');
+                    @endphp
 
                 <div class="relative z-20 mt-auto">
-                    <blockquote class="space-y-2">
-                        <flux:heading size="lg">&ldquo;{{ trim($message) }}&rdquo;</flux:heading>
-                        <footer><flux:heading>{{ trim($author) }}</flux:heading></footer>
-                    </blockquote>
+                    <div class="backdrop-blur-md bg-white/10 dark:bg-neutral-900/30 rounded-xl p-6 shadow-md border border-white/20 dark:border-neutral-800">
+                        <blockquote class="space-y-2">
+                            <flux:heading size="lg">&ldquo;{{ trim($message) }}&rdquo;</flux:heading>
+                            <footer><flux:heading>{{ trim($author) }}</flux:heading></footer>
+                        </blockquote>
+                    </div>
                 </div>
             </div>
             <div class="w-full lg:p-8">
